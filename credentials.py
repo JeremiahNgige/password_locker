c95e8a0b331f02fce3a0885267c3171f0cbfdd86
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-09-06 14:47:40 +0300

    credentials test

--- credentials.py
+++ credentials.py
@@ -1,2 +1,13 @@
 class Credentials:
+    '''
+    class that generates new instances of user accounts credentials
+    '''
+    credentials_list = []
     
+    def __init__(self,username_cred,password_cred):
+        '''
+        method to intialise the username and password cred of an account
+        '''
+        self.username_cred = username_cred
+        self.password_cred = password_cred
+        
--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,27 @@
 import unittest
 from credentials import Credentials
 
+class TestCredentials(unittest.TestCase):
+    '''
+    class to test all intances of the class Credentials
+    '''
+    
+    
+    def setUp(self):
+        '''
+        test to setup objects in the class 
+        '''
+        acc_name = input("enter username: ").casefold()
+        acc_password = input("enter password: ").casefold()
+    
+        self.new_credentials = Credentials(acc_name, acc_password)
+        
+    def test_init(self):
+        '''
+        test to check if instances are instantiated correctly
+        '''
+        self.assertEqual(self.new_credentials.username_cred, "mainjunior")
+        self.assertEqual(self.new_credentials.password_cred,"334455")
+        
+if __name__ == "__main__":
+    unittest.main()
