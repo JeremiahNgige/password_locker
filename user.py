bddd5fa1a085bb6bf3d2c1defaa927383f72d4a4
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-09-06 14:19:19 +0300

    user testing

--- user.py
+++ user.py
@@ -1,2 +1,14 @@
 class User:
+    ''' 
+    class that geneartes new instances of users
+    '''
+    user = None
+    
+    def __init__(self, username, password):
+        '''
+        method to initilaise username and password
+        '''
+        self.username = username
+        self.password = password
+        
     
--- user_test.py
+++ user_test.py
@@ -1,2 +1,25 @@
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    class to test every instance of the user class
+    '''
+    def setUp(self):
+        '''
+        method to create an instance of a user to test
+        '''
+        x = input("enter your username: ")
+        y = input("enter password: ")
+        
+        self.new_user = User(x,y)
+        
+    def test_init(self):
+        '''
+        test if user credentials are instantiated correctly
+        '''
+        self.assertEqual(self.new_user.username,"johndoe")
+        self.assertEqual(self.new_user.password,"232323")
+        
+if __name__ == "__main__":
+    unittest.main()
